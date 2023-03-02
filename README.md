9f5dd900f4fc3dfb66f7c711536bc5806a1436d8
Francisco Jose da Silva Sousa Junior <fco.silva.dev@gmail.com>
2023-03-02 11:34:52 +0300

    Add readme and minor changes

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-# Genius_Color_Game
-Replicando o jogo de cores Genius da Estrela
+# Genius Color Game
 
-Utilização de HTML, CSS e JavaScript. Tem como principais conceitos CSS Grid, manipulação de Array e Arrow Functions.
+Genius Color Game - A web-based game that challenges your memory and concentration skills.
+
+## Description
+
+This project is a web-based game inspired by the classic board game "Simon". The game challenges the player's memory and concentration skills by displaying a sequence of colored tiles, which the player must then repeat in the correct order. The game gets progressively more difficult as the sequence gets longer, and the player must complete the sequence without making a mistake.
+
+The game is built using HTML5, CSS3, and JavaScript, and utilizes modern web development techniques such as CSS Grid, array manipulation, and arrow functions.
+
+## Features
+
+- Interactive and engaging gameplay
+- Progressive difficulty levels
+- Responsive design for seamless mobile play
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Usage
+
+To play the game, simply clone this repository and open the `index.html` file in your preferred web browser. The game will load automatically and you can start playing immediately. Alternatively, you can play the game live by visiting the [GitHub Pages site](https://Burntroll.github.io/genius-color-game/).
+
+## Contributing
+
+Contributions to this project are welcome! Feel free to fork this repository and submit a pull request with your changes.
+
+## Credits
+
+This project was built by [Francisco Silva](https://github.com/Burntroll).
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
+## Results
+
+![website-photo](website.png)
--- js/script.js
+++ js/script.js
@@ -7,96 +7,94 @@ let score = 0;
 // 2 - amarelo
 // 3 - azul
 
-const blue = document.querySelector('.blue');
-const red = document.querySelector('.red');
-const green = document.querySelector('.green');
-const yellow = document.querySelector('.yellow');
-
+const blue = document.querySelector(".blue");
+const red = document.querySelector(".red");
+const green = document.querySelector(".green");
+const yellow = document.querySelector(".yellow");
 
 //cria ordem aleatória de cores
 let shuffleOrder = () => {
-    let colorOrder = Math.floor(Math.random() * 4);
-    order[order.length] = colorOrder;
-    clickedOrder = [];
+  let colorOrder = Math.floor(Math.random() * 4);
+  order[order.length] = colorOrder;
+  clickedOrder = [];
 
-    for(let i in order) {
-        let elementColor = createColorElement(order[i]);
-        lightColor(elementColor, Number(i) + 1)
-    }
-}
+  for (let i in order) {
+    let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
+  }
+};
 
 //acende a próxima cor
 let lightColor = (element, number) => {
-    number = number * 500;
-    setTimeout(() => {
-        element.classList.add('selected');
-    }, number - 250);
-    setTimeout(() => {
-        element.classList.remove('selected');
-    })
-}
+  number = number * 500;
+  setTimeout(() => {
+    element.classList.add("selected");
+  }, number - 250);
+  setTimeout(() => {
+    element.classList.remove("selected");
+  }, number);
+};
 
 //checa se os botões clicados são os mesmos da ordem gerada no jogo
 let checkOrder = () => {
-    for (let i in clickedOrder) {
-        if(clickedOrder[i] != order[i]) {
-            gameOver();
-            break;
-        }
+  for (let i in clickedOrder) {
+    if (clickedOrder[i] != order[i]) {
+      gameOver();
+      break;
     }
-    if (clickedOrder.length == order.length) {
-        alert(`Pontuação: ${score}\nVocê acertou! Iniciando o próximo nível!`);
-        nextLevel();
-    }
-}
+  }
+  if (clickedOrder.length == order.length) {
+    alert(`Score: ${score}\nYou got it! Starting the next level!`);
+    nextLevel();
+  }
+};
 
 //função para o clique do usuário
 let click = (color) => {
-    clickedOrder[clickedOrder.length] = color;
-    createColorElement(color).classList.add('selected');
+  clickedOrder[clickedOrder.length] = color;
+  createColorElement(color).classList.add("selected");
 
-    setTimeout(() => {
-        createColorElement(color).classList.remove('selected');
-        checkOrder();
-    }, 250);
-
-}
+  setTimeout(() => {
+    createColorElement(color).classList.remove("selected");
+    checkOrder();
+  }, 250);
+};
 
 //função que retorna a cor
 let createColorElement = (color) => {
-    if (color == 0) {
-        return green;
-    } else if (color == 1) {
-        return red;
-    } else if (color == 2) {
-        return yellow;
-    } else if (color == 3) {
-        return blue;
-    }
-}
+  if (color == 0) {
+    return green;
+  } else if (color == 1) {
+    return red;
+  } else if (color == 2) {
+    return yellow;
+  } else if (color == 3) {
+    return blue;
+  }
+};
 
 //Função para o próximo nível do jogo
 let nextLevel = () => {
-    score++;
-    shuffleOrder();
-}
+  score++;
+  shuffleOrder();
+};
 
 //Função game over
 let gameOver = () => {
-    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar novamente.`);
-    order = [];
-    clickedOrder = [];
+  alert(`Score: ${score}!\nYou lost the game!\nClick OK to start again.`);
+  order = [];
+  clickedOrder = [];
 
-    playGame();
-}
+  playGame();
+};
 
 //Função para iniciar o jogo
 let playGame = () => {
-    alert(`Bem vindo ao Gênesis! Iniciando novo jogo!`);
-    score = 0;
+  alert(`Welcome to Genesis! Starting a new game!`);
+  score = 0;
 
-    nextLevel();
-}
+  nextLevel();
+};
 
 //Eventos de clique para as cores
 green.onclick = () => click(0);
